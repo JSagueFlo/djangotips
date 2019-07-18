cf0bdcf6fe2ccf88e73fc1e99a229604c70af980
Joan Sagué Flo <joansf92@gmail.com>
2019-07-18 23:16:03 +0200

    added example urls

---
+++ example_urls.py
@@ -0,0 +1,45 @@
+"""projectname URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import include, path
+from django.conf.urls.i18n import i18n_patterns
+
+from django.contrib import admin
+from generic.views import HomeView
+
+# No translation
+urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+
+    path('',
+         HomeView.as_view(),
+         name="home"),
+]
+
+# International
+urlpatterns += i18n_patterns(
+    path('',
+         HomeView.as_view(),
+         name="home"),
+
+    path('admin/',
+         admin.site.urls,
+         name="admin_site"),
+
+    path('app1/', include('app1.urls')),
+
+    path('app2/', include('app2.urls')),
+
+)
